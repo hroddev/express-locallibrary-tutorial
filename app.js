dee204ecbdc09aff0d4fbd3704fa6927c52557d6
Hector Rodriguez <hector@hrod.dev>
2023-04-16 06:48:35 -0500

    add controllers

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var coolRouter = require('./routes/cool');
 
 var app = express();
 
@@ -37,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users/cool', coolRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
