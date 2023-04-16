f970e3ddfe88d57003f38de23269650e23506f76
Hector Rodriguez <hector@hrod.dev>
2023-04-16 14:57:35 -0500

    add author and book detail and controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -45,7 +45,21 @@ exports.book_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific book.
 exports.book_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+  const [book, bookinstance] = await Promise.all([
+    Book.findById(req.params.id).populate("author").populate("genre").exec(),
+    BookInstance.find({ book: req.params.id }).exec(),
+  ]);
+  if (book === null) {
+    const err = new Error("Book not found.");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("book_detail", {
+    title: book.title,
+    book: book,
+    book_instances: bookinstance,
+  });
 });
 
 // Display book create form on GET.
